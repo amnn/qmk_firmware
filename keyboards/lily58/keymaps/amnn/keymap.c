570749f17cddfcda47ab04b39f60c8a92ac301e6
Ashok Menon <ashokmenon@fb.com>
2021-08-22 21:09:40 +0100

    [lily58/amnn] Trackball tweaks
    - Scrolling mode changes colour of trackball RGB.
    - Mouse buttons on keyboard will force send a report.
    - Swap scrolling and right button -- makes it more convenient to hold
      the scroll button.

--- keyboards/lily58/keymaps/amnn/keymap.c
+++ keyboards/lily58/keymaps/amnn/keymap.c
@@ -47,7 +47,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                               KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_SLSH,
     KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                               KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
     KC_LSFT, KC_GRV,  KC_Z,    KC_X,    KC_C,    KC_V,    TB_BTN1,          XXXXXXX, KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_RSFT,
-                               KC_LALT, KC_LGUI, LOWER,   KC_SPC,           KC_ENT,  RAISE,   TB_BTN2, TB_SCRL
+                               KC_LALT, KC_LGUI, LOWER,   KC_SPC,           KC_ENT,  RAISE,   TB_SCRL, TB_BTN2
   ),
 
   [_UNSHIFT] = LAYOUT(
@@ -213,12 +213,19 @@ bool process_record_user(uint16_t keycode, keyrecord_t* record) {
   switch (keycode) {
     case TB_BTN1:
       trackball_register_button(record->event.pressed, MOUSE_BTN1);
+      pointing_device_send();
       return false;
     case TB_BTN2:
       trackball_register_button(record->event.pressed, MOUSE_BTN2);
+      pointing_device_send();
       return false;
     case TB_SCRL:
       trackball_set_scrolling(record->event.pressed);
+      if (record->event.pressed) {
+        trackball_set_rgbw(RGB_CYAN, 0x00);
+      } else {
+        trackball_set_rgbw(0x00, 0x00, 0x00, 0x4F);
+      }
       return false;
     default:
       return true;
